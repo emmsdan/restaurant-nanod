864ca02935686bfd065d03d279d46bb9324b49f8
EmmsDan <ecomje@gmail.com>
2018-07-13 14:57:14 +0100

    Updated Mapbox and google map

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZW1tc2RhbiIsImEiOiJjampocmJoc3gzaHloM2t0ZXk5bDdkZnRmIn0.ic9jOQF6PO2P-tTRpmaGgg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
