10ff1def03bfe29b4105f9d8b555d482f67ba679
Salem Media <ecomje@gmail.com>
2018-08-08 07:40:55 +0100

    Updated File

--- js/register.js
+++ js/register.js
@@ -1,8 +1,18 @@
+/*
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('sw.js')
   .then(registration => {
-      console.log(`Registration successful, scope is ${registration.scope}`);
+      console.log(`Registration successful, scope is '${registration.scope}'`);
   }).catch(error => {
-      console.log(`Service worker registration failed, error: ${error}`);
+      console.log(`Service worker registration failed, error: '${error}'`);
   });
 }
+*/
+
+window.addEventListener ('load', ()=>{
+    for (let rg of document.querySelectorAll('script[src]')){
+        console.log (rg)
+        console.log ()
+        console.log ()
+    }
+});
