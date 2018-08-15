417f8aca9486450e1ccb95406f390b4c61a88175
Salem Media <ecomje@gmail.com>
2018-08-15 22:13:27 +0100

    Updated Service Worker fetch event. To cache page visited by users@

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,42 +5,26 @@
 */
 
 const cacheVersion = '3.0';
-const cacheName = 's.io';
+const cacheName = 'emmsdan-restaurant.io';
 const cachNameVersion= `${cacheName}-static-${cacheVersion}`;
-const cachNameVersion2= `${cacheName}-app-${cacheVersion}`;
 /* add most basic files to be cached */
 const staticCache = [
+  './',
   './css/styles.css',
   './data/restaurants.json',
   './img/icon.png',
-  './img/',
   './js/dbhelper.js',
   './js/main.js',
   './js/restaurant_info.js',
-  './index.html',
   './restaurant.html'
 ];
-/* other files to be cached */
-const cachableFiles = [
-  'https://maps.googleapis.com/maps/api/js?key=AIzaSyBg46mssaV7VJ1N0H9nUrasFlX76IP4rgM&libraries=places&callback=initMap'
-]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     //add cache to system
     caches.open(cachNameVersion)
     .then((cache) => {
-        cache.addAll(staticCache);
-        //add cache to system
-        caches.open(cachNameVersion2)
-        .then((cache) => {
-          try {
-            cache.addAll(cachableFiles)
-          }catch (e) {
-            return 'Could not Access Server';
-          }
-        })
-
+      cache.addAll(staticCache);
     })
   );
 });
@@ -60,42 +44,31 @@ self.addEventListener('activate', (event) => {
 });
 
 /*
-  handles all request made by the browser.
+  let handles all request made by the browser.
 */
-self.addEventListener('fetch', (event) => {
-  let url = event.request.clone();
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-
-    event.respondWith(
-      fetch(event.request).catch(error => {
-        console.log('Oops, You are offline, Currently');
-        return caches.match('index.html');
-      })
-    );
-
-  }else{
-    event.respondWith(
-      caches.match(event.request)
-      .then((res) => {
-        if(res){
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true})
+    .then((response) => {
+      return response || fetch(event.request).then((res) => {
+        if(!res || res.status !== 200 || res.type !== 'basic'){
           return res;
         }
-        return fetch(url).then((res) => {
-          if(!res || res.status !== 200 || res.type !== 'basic'){
-            return res;
-          }
-          let response = res.clone();
-          caches.open(cachNameVersion)
-          .then((cache) => {
-            cache.put(event.request, response);
-          });
-          return res;
-        }).catch ((error)=> {
-          return error;
-        })
+        let response = res.clone();
+        caches.open(cachNameVersion)
+        .then((cache) => {
+          cache.put(event.request, response);
+        });
+        return res;
+      }).catch ((error)=> {
+        return error;
       })
-    )
-  }
+    })
+    .catch((err) => {
+      console.log(err, event.request)
+      return caches.match('index.html');
+    })
+  );
 });
 
 self.addEventListener('message', messageEvent => {
@@ -104,4 +77,4 @@ self.addEventListener('message', messageEvent => {
 
 self.addEventListener('controllerchange', () => {
     window.location.assign('./');
-})
+});
